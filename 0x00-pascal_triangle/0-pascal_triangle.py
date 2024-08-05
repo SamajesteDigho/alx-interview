a49a668c2005c75d149f29d946a6210b27d91416
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-05 14:59:27 +0800

    Fisrt version of Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    In this module file we define a function used to print the pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """ Given an integer n, we write down the Pascal Triangle of this one """
+    pass
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+## 0x00-pascal_triangle
+
+The pascal triangle Algorithm
