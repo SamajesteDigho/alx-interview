7ecb6fd3c2e1f11d60ae3d52d6fbcd1e17fea2a1
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-05 15:47:26 +0800

    Pushing full version of pascal traingle algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,27 @@
 """
 
 
-def pascal_triangle(n):
+def get_new_row(prev_row: list, level: int) -> list:
+    """ Get the previous row and return the new row """
+    if level == 0:
+        return [1]
+    new = [1]
+    size = len(prev_row)
+    for x in range(0, size):
+        if x <= size - 2:
+            new.append(prev_row[x] + prev_row[x + 1])
+    new.append(1)
+    return new
+
+
+def pascal_triangle(n) -> list:
     """ Given an integer n, we write down the Pascal Triangle of this one """
-    pass
+    if n <= 0:
+        return []
+    pascaline = []
+    row = []
+    for level in range(n):
+        row = get_new_row(row, level)
+        pascaline.append(row)
+
+    return pascaline
