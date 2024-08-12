56eb27c500d3203645e4a7e31a7c0ef38bd66c2a
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-12 14:57:12 +0800

    Comiting Exo 0x01 LockBoxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    This code module permits us to tacle the problem of lock blocks
+"""
+
+
+def markBlockAsVisited(index, stv, arv):
+    if stv.__contains__(index):
+        stv.remove(index)
+    arv.add(index)
+    return stv, arv
+
+
+def visitNodes(boxes, index, stv, arv):
+    """ Nodes Visiting contest """
+    stv, arv = markBlockAsVisited(index, stv=stv, arv=arv)
+    if len(boxes[index]) == 0:
+        return stv, arv
+
+    for x in boxes[index]:
+        if not arv.__contains__(x):
+            stv, arv = visitNodes(boxes, x, stv, arv)
+    return stv, arv
+
+
+def canUnlockAll(boxes) -> bool:
+    """
+        This function determines if all blocks can be opened
+        and return True or False
+    """
+    remain = set([x for x in range(len(boxes))])
+    visited = set()
+    remain, visited = visitNodes(boxes, 0, stv=remain, arv=visited)
+
+    if len(remain) == 0:
+        return True
+    else:
+        return False
