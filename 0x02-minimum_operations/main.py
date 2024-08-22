aa99e47801966c49f525a9b28551b6b741cc72fa
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-23 06:54:52 +0800

    Correcting and pushing new version

--- 0x02-minimum_operations/main.py
+++ 0x02-minimum_operations/main.py
@@ -4,10 +4,7 @@ Main file for testing
 """
 minOperations = __import__('0-minoperations').minOperations
 
-
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 8
+n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
