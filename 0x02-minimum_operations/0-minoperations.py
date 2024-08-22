0cb942cf05e821fe139ac06bcb2b3f337924e1ee
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-23 06:29:02 +0800

    Correcting and pushing new version

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def fragment_number(n: int) -> Tuple[int, int]:
     """ Fragment a given number into the 2 closest multiple """
     pair: Tuple = tuple([1, n])
-    for i in range(2, n//2 + 1):
+    for i in range(2, n//2):
         mod: int = n % i
         if mod == 0 and abs(i - n//i) < abs(pair[0] - pair[1]):
             pair = tuple([i, n//i])
@@ -28,5 +28,7 @@ def process_node(node: Tuple[int, int]) -> int:
 
 def minOperations(n: int) -> int:
     """ Here the principal function for getting the number of operations """
+    if n <= 1:
+        return 0
     initial: Tuple[int, int] = fragment_number(n=n)
     return process_node(node=initial)
