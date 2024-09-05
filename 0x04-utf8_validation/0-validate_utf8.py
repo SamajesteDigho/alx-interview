a742762c4f528f5f0f42e31430ce79e5edea6cf9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-05 20:21:33 +0800

    Commiting exercise

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    UTF8 Bit vlidation exercise
+"""
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """ Validate data as utf8 character """
+    for x in data:
+        if x <0 or x >= 256:
+            return False
+    return True
