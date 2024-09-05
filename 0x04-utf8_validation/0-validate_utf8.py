88355e37584a39976381de06dd40a6e7a1bef046
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-05 20:23:05 +0800

    Commiting exercise

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,6 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """ Validate data as utf8 character """
     for x in data:
-        if x <0 or x >= 256:
+        if x < 0 or x >= 256:
             return False
     return True
