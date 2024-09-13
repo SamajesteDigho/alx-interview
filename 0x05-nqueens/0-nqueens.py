84771e3c8158da8fc22c5af9f2dfd88e894bec5a
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-13 10:02:51 +0800

    Commiting

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,8 +6,9 @@ from sys import argv
 
 
 class Cheese:
-    
-    def __init__(self,size=4):
+    """ The chese class """
+    def __init__(self, size=4):
+        """ Initialisation """
         self.board = None
         self.queens = []
         self.possibilities = []
@@ -15,6 +16,7 @@ class Cheese:
         self.N = size
 
     def check_collision(self, pos_x, pos_y):
+        """ Collision checking """
         # vertical collision
         for x in self.queens:
             if x[1] == pos_y:
@@ -53,17 +55,18 @@ class Cheese:
                 x += 1
                 y -= 1
         return False
-    
+
     def back_propagation(self, row: int, col: int) -> bool:
+        """ Backward propagation """
         # TODO: Check ending conditions
         if row >= self.N or col >= self.N:
             return False
         if self.check_collision(pos_x=row, pos_y=col):
-            print("Collision found ({}, {})".format(row, col))
-            for i in  range(col, N):
+            # print("Collision found ({}, {})".format(row, col))
+            for i in range(col, N):
                 self.back_propagation(row, i)
         else:
-            print("No Collision found ({}, {})".format(row, col))
+            # print("No Collision found ({}, {})".format(row, col))
             self.queens.append((row, col))
             self.back_propagation(row+1, col)
         if len(self.queens) == N:
@@ -72,34 +75,35 @@ class Cheese:
             self.failed_paths.append(self.queens.copy())
         if len(self.queens) > 0:
             self.queens.pop()
-        
+
     def search_n_queens_dispositions(self):
+        """ Searching n queens function parser """
         for col in range(self.N):
             self.queens = []
             self.back_propagation(0, col)
-    
+
     @staticmethod
     def print_list_object(list_obj):
+        """ Printing the list of the queens """
         print("Printing List object")
         for x in list_obj:
             print("{}".format(x))
 
     def __str__(self):
+        """ Stringifying """
         string = "CHEESE BOARD INFOS\n"
         string += "\tBoard Size: {}\n".format(self.N)
         string += "\tACTUAL QUEENS DISPOSITIONS: {}\n".format(len(self.queens))
         for x in self.queens:
             string += "\t\t{}".format(x)
-        string += "\n\tN-QUEENS DISPOSITIONS: {}\n".format(len(self.possibilities))
+        string += "\n\tN-QUEENS DISPOSITIONS: {}\n".format(
+            len(self.possibilities))
         for x in self.possibilities:
             string += "\t\t{}".format(x)
         string += "\n\tFAILED: {}\n".format(len(self.failed_paths))
         for x in self.failed_paths:
             string += "\t\t{}\n".format(x)
         return string.strip()
-    
-        
-
 
 
 if __name__ == "__main__":
@@ -114,9 +118,9 @@ if __name__ == "__main__":
     if N < 4:
         print("N must be at least 4")
         exit(1)
-    
+
     cheese = Cheese(size=N)
-    
+
     print("=====================================")
     cheese.search_n_queens_dispositions()
     print("=====================================")
