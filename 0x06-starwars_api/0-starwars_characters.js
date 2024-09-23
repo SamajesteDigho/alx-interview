8a85c8b84649a8a3013b1a8cc15961473f447e2d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-23 14:43:39 +0800

    Commiting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,23 +7,39 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-function fetchNamePromises (url) {
-  return new Promise(() => {
-    request(url, (err, response, body) => {
-      if (!err && response.statusCode === 200) {
-        const data = JSON.parse(body);
-        console.log(data.name);
-      }
+async function fetchUrlsInOrder (urls) {
+  const promises = urls.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error && response.statusCode === 200) {
+          reject(error);
+        } else {
+          const data = JSON.parse(body).name;
+          resolve(data);
+        }
+      });
     });
   });
+  try {
+    return await Promise.all(promises);
+  } catch (error) {
+    return [];
+  }
 }
 
-request(url, async (err, response, body) => {
-  if (!err && response.statusCode === 200) {
+request(url, (error, response, body) => {
+  if (error && response.statusCode === 200) {
+    console.log(`Error: ${error}`);
+  } else {
     const characters = JSON.parse(body).characters;
-    const promises = [];
-    characters.forEach(elt => {
-      promises.push(fetchNamePromises(elt));
-    });
+    fetchUrlsInOrder(characters)
+      .then(results => {
+        results.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 });
