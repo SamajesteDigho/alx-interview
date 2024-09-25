86ff95be8a10893bd2ebb1af9f0eeed8582c67b6
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-25 15:21:24 +0800

    Commiting

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Rotating 2D Matrix module
+"""
+from typing import Any, List
+
+
+def check_is_matrix(matrix: Any) -> bool:
+    """ Check if a matrix is squared """
+    if not isinstance(matrix, list):
+        return False
+    elif not all(map(lambda x: isinstance(x, list), matrix)):
+        return False
+    return True
+
+
+def check_is_squared(matrix: List[List[Any]]) -> bool:
+    if not isinstance(matrix, list):
+        return False
+    if len(matrix) == 1:
+        return True
+    length = len(matrix[0])
+    if not all(map(lambda x: len(x) == length, matrix)):
+        return False
+    return True
+
+
+def rotate_2d_matrix(matrix: List[List[Any]]):
+    """ 2D Rotation of Matrix """
+    if check_is_matrix(matrix=matrix) and check_is_squared(matrix=matrix):
+        size = len(matrix[0])
+        new = [[] for _ in range(size)]
+        for i in range(size):
+            for j in range(size):
+                new[j].append(matrix[i][j])
+        matrix = new
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,3 @@
+## 0x07-rotate_2d_matrix
+
+Here we go 0x07-rotate_2d_matrix
