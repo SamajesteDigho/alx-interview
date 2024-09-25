f217cb5e6e62bfe435e4d194ba031101f45cc1e9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-25 15:23:49 +0800

    Commiting

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -28,8 +28,8 @@ def rotate_2d_matrix(matrix: List[List[Any]]):
     """ 2D Rotation of Matrix """
     if check_is_matrix(matrix=matrix) and check_is_squared(matrix=matrix):
         size = len(matrix[0])
-        new = [[] for _ in range(size)]
         for i in range(size):
             for j in range(size):
-                new[j].append(matrix[i][j])
-        matrix = new
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = tmp
