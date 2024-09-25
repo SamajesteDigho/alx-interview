ca6f6f8445b41efb274f1aafadda020666084e87
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-25 15:36:49 +0800

    Commiting

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -28,7 +28,5 @@ def rotate_2d_matrix(matrix: List[List[Any]]):
     """ 2D Rotation of Matrix """
     size = len(matrix[0])
     for i in range(size):
-        for j in range(size):
-            tmp = matrix[i][j]
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = tmp
+        for j in range(i, size):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
