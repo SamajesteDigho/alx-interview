1bfe6a718369a83227e57e8f936a2ef2e96f26f2
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-27 09:06:33 +0800

    Commiting

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,19 +4,9 @@
 from typing import Any, List
 
 
-def anti_clock_wise_rotation(matrix: List[List[Any]], size: int):
-    """ Anti-clockwise rotation """
-    for x in range(0, int(size / 2)):
-        for y in range(x, size-x-1):
-            tmp = matrix[x][y]
-            matrix[x][y] = matrix[y][size-1-x]
-            matrix[y][size-1-x] = matrix[size-1-x][size-1-y]
-            matrix[size-1-x][size-1-y] = matrix[size-1-y][x]
-            matrix[size-1-y][x] = tmp
-
-
-def clock_wise_rotation(matrix: List[List[Any]], size: int):
-    """ Clock wise rotation """
+def rotate_2d_matrix(matrix: List[List[Any]]):
+    """ 2D Rotation of Matrix """
+    size = len(matrix[0])
     for x in range(0, int(size / 2)):
         for y in range(x, size-x-1):
             tmp = matrix[x][y]
@@ -24,9 +14,3 @@ def clock_wise_rotation(matrix: List[List[Any]], size: int):
             matrix[size-1-y][x] = matrix[size-1-x][size-1-y]
             matrix[size-1-x][size-1-y] = matrix[y][size-1-x]
             matrix[y][size-1-x] = tmp
-
-
-def rotate_2d_matrix(matrix: List[List[Any]]):
-    """ 2D Rotation of Matrix """
-    size = len(matrix[0])
-    clock_wise_rotation(matrix=matrix, size=size)
---
+++ 0x07-rotate_2d_matrix/alls.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Rotating 2D Matrix module
+"""
+from typing import Any, List
+
+
+def anti_clock_wise_rotation(matrix: List[List[Any]], size: int):
+    """ Anti-clockwise rotation """
+    for x in range(0, int(size / 2)):
+        for y in range(x, size-x-1):
+            tmp = matrix[x][y]
+            matrix[x][y] = matrix[y][size-1-x]
+            matrix[y][size-1-x] = matrix[size-1-x][size-1-y]
+            matrix[size-1-x][size-1-y] = matrix[size-1-y][x]
+            matrix[size-1-y][x] = tmp
+
+
+def clock_wise_rotation(matrix: List[List[Any]], size: int):
+    """ Clock wise rotation """
+    for x in range(0, int(size / 2)):
+        for y in range(x, size-x-1):
+            tmp = matrix[x][y]
+            matrix[x][y] = matrix[size-1-y][x]
+            matrix[size-1-y][x] = matrix[size-1-x][size-1-y]
+            matrix[size-1-x][size-1-y] = matrix[y][size-1-x]
+            matrix[y][size-1-x] = tmp
