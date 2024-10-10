2025728a616e3975c3a3e86a8ca49e7b9c1c1134
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-10 18:12:02 +0800

    Commiting

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Module perimeter of the island
+"""
+
+
+def get_perimeter(grid, row, col):
+    """ Get the perimeter of a square """
+    if grid[row][col] == 0:
+        return 0
+    else:
+        count = 0
+        try:
+            if grid[row-1][col] == 0:
+                count += 1
+        except IndexError:
+            count += 1
+        try:
+            if grid[row+1][col] == 0:
+                count += 1
+        except IndexError:
+            count += 1
+        try:
+            if grid[row][col-1] == 0:
+                count += 1
+        except IndexError:
+            count += 1
+        try:
+            if grid[row][col+1] == 0:
+                count += 1
+        except IndexError:
+            count += 1
+        return count
+
+
+def island_perimeter(grid):
+    """ Let's get the perimeter of the island """
+    n = len(grid)
+    m = len(grid[0])
+    perimeter = 0
+    for i in range(n):
+        for j in range(m):
+            perimeter += get_perimeter(grid, i, j)
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,3 @@
+## 0x09-island_perimeter
+
+Readme file for the island island_perimeter
