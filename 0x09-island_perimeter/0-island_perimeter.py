6caad086f0e86bf4a938b0ed6ffe1db3148013d3
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-14 06:45:51 +0800

    Commiting

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,31 +3,31 @@
 """
 
 
-def get_perimeter(grid, row, col):
+def get_perimeter(grid, size, row, col):
     """ Get the perimeter of a square """
     if grid[row][col] == 0:
         return 0
     else:
         count = 0
-        try:
-            if grid[row-1][col] == 0:
-                count += 1
-        except IndexError:
+        # Check West direction
+        if row - 1 < 0:
             count += 1
-        try:
-            if grid[row+1][col] == 0:
-                count += 1
-        except IndexError:
+        elif grid[row-1][col] == 0:
             count += 1
-        try:
-            if grid[row][col-1] == 0:
-                count += 1
-        except IndexError:
+        # Check East direction
+        if row + 1 >= size[0]:
             count += 1
-        try:
-            if grid[row][col+1] == 0:
-                count += 1
-        except IndexError:
+        elif grid[row+1][col] == 0:
+            count += 1
+        # Check North direction
+        if col - 1 < 0:
+            count += 1
+        elif grid[row][col-1] == 0:
+            count += 1
+        # Check South direction
+        if col + 1 >= size[1]:
+            count += 1
+        elif grid[row][col+1] == 0:
             count += 1
         return count
 
@@ -44,7 +44,8 @@ def island_perimeter(grid):
     try:
         for i in range(n):
             for j in range(m):
-                perimeter += get_perimeter(grid, i, j)
-    except Exception:
+                perimeter += get_perimeter(grid, (n, m), i, j)
+    except Exception as e:
+        print(e)
         return 0
     return perimeter
